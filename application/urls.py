fa29e8e626a06c7dafb1dabdfd7e7988cc259874
Timoh97 <timothy.nthiga@student.moringaschool.com>
2021-12-29 23:56:38 +0300

    create authentication system

--- application/urls.py
+++ application/urls.py
@@ -1,5 +1,6 @@
 
-  
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -7,4 +8,11 @@ from . import views
 
 urlpatterns = [
 path('',views.index, name='index'),
+path('home/',views.home, name='home'),
+path("profile/", views.profile, name="profile"), # profile page
+path("profile/update/", views.update_profile, name="update_profile"), # profile update page
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
